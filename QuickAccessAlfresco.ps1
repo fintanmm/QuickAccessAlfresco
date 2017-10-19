eff87bcc34d04edf8c9c8da48a46d374b0f6df95
Fintan MacMahon <fintan.macmahon@gmail.com>
2017-10-19 17:15:45 +0100

    Use authenticated web request instead of invoke-webrequest

--- QuickAccessAlfresco.ps1
+++ QuickAccessAlfresco.ps1
@@ -14,7 +14,10 @@ function Build-Url([String] $urlParams="") {
 }
 
 function Get-ListOfSites([String] $url) {
-    return Invoke-WebRequest -Uri $url -TimeoutSec 10 | ConvertFrom-Json
+    $webclient = new-object System.Net.WebClient
+    $webclient.UseDefaultCredentials=$true
+    return $webclient.DownloadString($url)  | ConvertFrom-Json
+    # return Invoke-WebRequest -Uri $url -TimeoutSec 10 | ConvertFrom-Json
 }
 
 function Create-HomeAndSharedLinks {
