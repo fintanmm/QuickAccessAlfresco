725d0f35552c31b7d4d2e654967c1dee9cf1ff0a
Tudor Chiribes <tudor.chiribes@gmail.com>
2018-01-26 12:10:17 +0000

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,7 @@
-powershell.exe Invoke-Pester .\QuickAccessAlfresco.Tests.ps1 -CodeCoverage .\QuickAccessAlfresco.ps1
+SET whoAmI=%USERNAME%
+SET webDir=alfresco\service\api\people\%whoAmI%\sites\
+mkdir %webDir%
+copy stub\sites.json %webDir%\index.json
+type NUL > alfresco_careers_icon.ico
+powershell.exe "& ".\server.ps1"" 
+powershell.exe -noexit Invoke-Pester .\QuickAccessAlfresco.Tests.ps1 -CodeCoverage .\QuickAccessAlfresco.ps1
