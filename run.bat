bf0601dc33348933575a2ea6de59e5df0e69e130
Fintan MacMahon <fintan.macmahon@gmail.com>
2018-02-07 13:12:39 +0000

    Start powershell as admin so that the certs can be added for https connection

--- run.bat
+++ run.bat
@@ -3,5 +3,5 @@ SET webDir=alfresco\service\api\people\%whoAmI%\sites\
 mkdir %webDir%
 copy stub\sites.json %webDir%\index.json
 type NUL > alfresco_careers_icon.ico
-powershell.exe "& ".\server.ps1"" 
+powershell.exe "Start-Process powershell.exe .\server.ps1 -Verb runAs" 
 powershell.exe -noexit Invoke-Pester .\QuickAccessAlfresco.Tests.ps1 -CodeCoverage .\QuickAccessAlfresco.ps1
