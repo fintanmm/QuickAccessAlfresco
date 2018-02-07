0ca1648d704364b503fc0046d864ac3379aade62
Fintan MacMahon <fintan.macmahon@gmail.com>
2018-02-07 13:14:54 +0000

    Add a cert so that we can test against https

--- server.ps1
+++ server.ps1
@@ -1,13 +1,28 @@
-$Hso = New-Object Net.HttpListener
-$Hso.Prefixes.Add("http://localhost:8080/")
-$Hso.Start()
-While ($Hso.IsListening) {
-    $HC = $Hso.GetContext()
-    $HRes = $HC.Response
-    $HRes.Headers.Add("Content-Type","application/jsonn")
-    $Buf = [Text.Encoding]::UTF8.GetBytes((GC (Join-Path $Pwd ($HC.Request).RawUrl)))
-    $HRes.ContentLength64 = $Buf.Length
-    $HRes.OutputStream.Write($Buf,0,$Buf.Length)
-    $HRes.Close()
+# Use the following commands to bind/unbind SSL cert
+netsh http add sslcert hostnameport="localhost:8443" certhash="7494771BBD7F5287B18B1190E6A0CEFB00794DA4" appid='{811909a4-05f0-44cc-81e4-782854473183}' certstorename=my
+$HttpListener = New-Object System.Net.HttpListener
+if(-NOT $HttpListener.IsListening){
+  $HttpListener.Prefixes.Add("http://localhost:8080/")
+  $HttpListener.Prefixes.Add("https://localhost:8443/")
+  $HttpListener.Start()  
+  While ($HttpListener.IsListening) {
+      $HttpContext = $HttpListener.GetContext()
+      $HttpRequest = $HttpContext.Request
+      $RequestUrl = $HttpRequest.Url.OriginalString
+      Write-Output "$RequestUrl"
+      if($HttpRequest.HasEntityBody) {
+        $Reader = New-Object System.IO.StreamReader($HttpRequest.InputStream)
+        Write-Output $Reader.ReadToEnd()
+      }
+      $HttpResponse = $HttpContext.Response
+      $HttpResponse.Headers.Add("Content-Type","application/json")
+      $HttpResponse.StatusCode = 200
+      $ResponseBuffer = [System.Text.Encoding]::UTF8.GetBytes("")
+      $HttpResponse.ContentLength64 = $ResponseBuffer.Length
+      $HttpResponse.OutputStream.Write($ResponseBuffer,0,$ResponseBuffer.Length)
+      $HttpResponse.Close()
+      Write-Output "" # Newline
+  }
+  $HttpListener.Stop()
 }
-$Hso.Stop()
+netsh http delete sslcert ipport="localhost:8443"
