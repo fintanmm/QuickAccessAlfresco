62afb7b0d7346a93e5270f9ef3c979d4ece82523
Tudor Chiribes <tudor.chiribes@gmail.com>
2018-02-07 14:50:19 +0000

    resolved run.bat conflict

--- run.bat
+++
@@ -1,7 +0,0 @@
-SET whoAmI=%USERNAME%
-SET webDir=alfresco\service\api\people\%whoAmI%\sites\
-mkdir %webDir%
-copy stub\sites.json %webDir%\index.json
-type NUL > alfresco_careers_icon.ico
-powershell.exe Start-Job {.\server.ps1} 
-powershell.exe -noexit Invoke-Pester .\QuickAccessAlfresco.Tests.ps1 -CodeCoverage .\QuickAccessAlfresco.ps1
