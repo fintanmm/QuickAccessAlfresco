4bea0223418bda8a80f3baae1c6bcf9b8f861555
Fintan MacMahon <fintan.macmahon@gmail.com>
2018-02-09 15:46:50 +0000

    copy json file as is

--- run.ps1
+++ run.ps1
@@ -2,6 +2,7 @@ $whoAmI = $env:UserName
 $webDir = "alfresco\service\api\people\%whoAmI%\sites\"
 New-Item -ItemType Directory -Force -Path $webDir
 Copy-Item "stub\sites.json" "$webDir\index.json"
+Copy-Item "stub\sites.json" "$webDir\"
 New-Item -Name quickaccess_icon.ico  -Force -ItemType File
 Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "$pwd\server.ps1" -Verb runas
 Invoke-Pester .\QuickAccessAlfresco.Tests.ps1 -CodeCoverage .\QuickAccessAlfresco.ps1
