5352178d9579b2bf48281c595857a7ec9a65cb9d
Fintan MacMahon <fintan.macmahon@gmail.com>
2018-02-09 11:49:42 +0000

    No longer need batch script

--- run.bat
+++
@@ -1,7 +0,0 @@
-SET whoAmI=%USERNAME%
-SET webDir=alfresco\service\api\people\%whoAmI%\sites\
-mkdir %webDir%
-copy stub\sites.json %webDir%\index.json
-type NUL > quickaccess_icon.ico
-powershell.exe "Start-Process -FilePath `"powershell.exe`" -ArgumentList `"-NoExit`", `"$pwd\server.ps1`" -Verb runas"
-REM powershell.exe -noexit Invoke-Pester .\QuickAccessAlfresco.Tests.ps1 -CodeCoverage .\QuickAccessAlfresco.ps1
