e93acfa94d35f3a73c7e03e7bdbae6c54f6b19a3
Fintan MacMahon <fintan.macmahon@gmail.com>
2018-02-12 11:26:49 +0000

    Fixed string interpolation

--- run.ps1
+++ run.ps1
@@ -1,8 +1,8 @@
 $whoAmI = $env:UserName
-$webDir = "alfresco\service\api\people\%whoAmI%\sites\"
+$webDir = "alfresco\service\api\people\$whoAmI\sites\"
 New-Item -ItemType Directory -Force -Path $webDir
 Copy-Item "stub\sites.json" "$webDir\index.json"
-Copy-Item "stub\sites.json" "$webDir\"
+Copy-Item "stub\sites.json" "$webDir\sites.json"
 New-Item -Name quickaccess_icon.ico  -Force -ItemType File
 Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "$pwd\server.ps1" -Verb runas
 Invoke-Pester .\QuickAccessAlfresco.Tests.ps1 -CodeCoverage .\QuickAccessAlfresco.ps1
