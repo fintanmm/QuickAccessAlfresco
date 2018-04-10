b3481bf5364503003bffb73d9ce0d6e05d4702bd
Fintan MacMahon <fintan.macmahon@gmail.com>
2018-04-10 11:27:32 +0100

    Workaround for the webserver causing one test to fail.

--- run.ps1
+++ run.ps1
@@ -1,8 +1,14 @@
+if (Test-Path ".\0)") {
+    Remove-Item -Path .\0
+}
 $whoAmI = $env:UserName
 $webDir = "share\proxy\alfresco\api\people\$whoAmI\sites\"
-New-Item -ItemType Directory -Force -Path $webDir
+$webDavDir = "alfresco\webdav\Sites"
+New-Item -ItemType Directory -Force -Path $webDir $webDavDir
 Copy-Item "stub\sites.json" "$webDir\index.json"
 Copy-Item "stub\sites.json" "$webDir\sites.json"
 New-Item -Name quickaccess_icon.ico  -Force -ItemType File
 Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "$pwd\server.ps1" -Verb runas
+$whoAmI = $env:UserName
+Invoke-WebRequest "https://localhost:8443/share/proxy/alfresco/api/people/$whoAmI/sites/sites.json"
 Invoke-Pester .\QuickAccessAlfresco.Tests.ps1 -CodeCoverage .\QuickAccessAlfresco.ps1
