6c9a8ba658ba72c5ce19a92736c1a546a17b437d
Fintan MacMahon <fintan.macmahon@gmail.com>
2018-04-24 11:41:51 +0100

    Copy stub json to SP dir

--- run.ps1
+++ run.ps1
@@ -4,9 +4,12 @@ if (Test-Path ".\0)") {
 $whoAmI = $env:UserName
 $webDir = "share\proxy\alfresco\api\people\$whoAmI\sites\"
 $webDavDir = "alfresco\webdav\Sites"
+$spDir = "alfresco\aos\Sites"
 New-Item -ItemType Directory -Force -Path $webDir 
 New-Item -ItemType Directory -Force -Path $webDavDir
+New-Item -ItemType Directory -Force -Path $spvDir
 Copy-Item "stub\sites.json" "$webDavDir\index.json"
+Copy-Item "stub\sites.json" "$spDir\index.json"
 Copy-Item "stub\sites.json" "$webDir\index.json"
 Copy-Item "stub\sites.json" "$webDir\sites.json"
 New-Item -Name quickaccess_icon.ico  -Force -ItemType File
