d32c7c7465e27356bfcd7b2c5a68e96a8a130211
Fintan MacMahon <fintan.macmahon@gmail.com>
2018-07-09 12:31:08 +0100

    Initial support for psake build sys

---
+++ psakefile.ps1
@@ -0,0 +1,48 @@
+$whoAmI = $env:UserName
+$webDir = "share\proxy\alfresco\api\people\$whoAmI\sites\"
+$webDavDir = "alfresco\webdav\Sites"
+$spDir = "alfresco\aos\Sites"
+
+TaskSetup {
+    "Executing task setup"
+    New-Item -ItemType Directory -Force -Path $webDir 
+    New-Item -ItemType Directory -Force -Path $webDavDir
+    New-Item -ItemType Directory -Force -Path $spDir
+    Copy-Item "stub\sites.json" "$webDavDir\index.json"
+    Copy-Item "stub\sites.json" "$spDir\index.json"
+    Copy-Item "stub\sites.json" "$webDir\index.json"
+    Copy-Item "stub\sites.json" "$webDir\sites.json"
+    New-Item -Name quickaccess_icon.ico -Force -ItemType File    
+}
+  
+TaskTearDown {
+    "Executing task tear down"
+    # Remove-Item -Path "$webDavDir"
+    # Remove-Item -Path "$spDir"
+    # Remove-Item -Path "$webDir"
+}
+
+Task default -depends InvokePester
+
+Task InvokePester -depends GetSiteJson{
+    "Invoke Pester with Coverage"
+    Invoke-Pester .\QuickAccessAlfresco.Tests.ps1 -CodeCoverage .\QuickAccessAlfresco.ps1
+}
+
+Task GetSiteJson {
+    "GetSiteJson executed"
+    Invoke-WebRequest "https://localhost:8443/share/proxy/alfresco/api/people/$whoAmI/sites/sites.json"    
+}
+
+Task Lint {
+    "Linting"
+    Invoke-ScriptAnalyzer -Path .\QuickAccessAlfresco.ps1 -Setting .\ScriptAnalyzerSettings.psd1
+}
+
+Task -Name Formatter -Description "Format code" {
+    Invoke-Formatter .\QuickAccessAlfresco.ps1 -Settings .\ScriptAnalyzerSettings.psd1
+}
+
+Task -Name ConCatFiles -Depends InvokePester -Description "Concatenates files into one file"{
+    # cat params.ps1,user.ps1,links.ps1,task.ps1,icon.ps1,config.ps1,main.ps1 | sc .\QuickAccessAlfresco.ps1
+}
