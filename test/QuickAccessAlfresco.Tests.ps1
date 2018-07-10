23610cfd35fa9ae58cbf81b9655b72164411989d
Fintan MacMahon <fintan.macmahon@gmail.com>
2018-07-10 13:06:54 +0100

    Fixed path

--- test/QuickAccessAlfresco.Tests.ps1
+++ test/QuickAccessAlfresco.Tests.ps1
@@ -1,8 +1,6 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-. "$here\$sut"
-
-.".\QuickAccessAlfresco.ps1"
+. "$here\..\src\$sut"
 
 $whoAmI = $env:UserName
 $linkBaseDir = "$env:userprofile\Links"
