fd9bd69ddc48f71f1a11781fc8af4b93fc8a82bb
Fintan MacMahon <fintan.macmahon@gmail.com>
2018-07-10 14:21:59 +0100

    Adding test and code

---
+++ test/main.Test.ps1
@@ -0,0 +1,13 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\fixtures.ps1"
+. "$here\..\src\$sut"
+
+Describe "Check-PSVersion" {
+    It "Should check if PowerShell version is 3 or higher."{
+        $psVersion = Check-PSVersion
+        $psVersion | Should be $true
+    }
+}
+
+
