0758b5933ab1d5a62c1b027b3d47a8d418b87058
Fintan MacMahon <fintan.macmahon@gmail.com>
2018-07-10 13:58:24 +0100

    Adding config code

---
+++ src/config.ps1
@@ -0,0 +1,31 @@
+function Generate-Config ($fromParams=@{}) {
+    $doesConfigExist = Test-Path "$appData\config.json"
+    if(!$doesConfigExist){
+        $fromParams | ConvertTo-Json | Set-Content "$appData\config.json"
+        return $true
+    }
+    return $false
+}
+
+function Parse-Config {
+    $getConfigContent = Read-Config
+    $switches = $getConfigContent.switches
+    $parseSwitches = ""
+    $switches.psobject.properties.name | ForEach-Object{
+        $parseSwitches += "-{0} '{1}' " -f $_, $switches.$_
+    }
+
+    $sites = $getConfigContent.sites
+    $parseSites = @(0) * $sites.Count
+    for ($i = 0; $i -lt $sites.Count; $i++) {
+        if ($sites[$i].title) {
+            $parseSites[$i] = $sites[$i].title
+        }
+    }
+    return @{"switches" = $parseSwitches; "sites" = $parseSites;}
+}
+
+function Read-Config {
+    $getConfigContent = Get-Content -Path "$appData\config.json" | Out-String | ConvertFrom-Json
+    return $getConfigContent    
+}
