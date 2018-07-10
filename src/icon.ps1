b7e961c060ac826c2122297d46d06dd015607358
Fintan MacMahon <fintan.macmahon@gmail.com>
2018-07-10 14:19:10 +0100

    Adding code

---
+++ src/icon.ps1
@@ -0,0 +1,8 @@
+function CopyIcon($icon="") {
+    $testPath = (-Not (Test-Path "$appData\$icon"))
+    if ($icon -And $testPath) {
+        Copy-Item $icon "$appData\"
+        return $true
+    }
+    return $false
+}
